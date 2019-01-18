1c90306e0ee10a4131a48f4ba19c1435a7a5649f
Bhanu Teja <pbteja1998@gmail.com>
2019-01-18 18:29:20 +0530

    initial commit

---
+++ packages/Address/webpack.config.js
@@ -0,0 +1,47 @@
+let path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader','css-loader',"less-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    externals: {
+        'react': 'commonjs react'
+    }
+};
